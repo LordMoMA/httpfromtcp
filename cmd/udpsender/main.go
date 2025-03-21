9dc70c66c2a63b44dc2ed069ddf5de08ad2ab0ff
LordMoMA <lihongyi0512@gmail.com>
2025-03-20 22:30:37 -0400

    buffer reuse

--- cmd/udpsender/main.go
+++ cmd/udpsender/main.go
@@ -21,22 +21,26 @@ func main() {
 	}
 	defer conn.Close()
 
-	for {
+	reader := bufio.NewReader(os.Stdin)
 
+	for {
+		// Print a prompt character
 		fmt.Print("> ")
 
-		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Read a line from stdin
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
+		// Write the line to the UDP connection
 		count, err := conn.Write([]byte(line))
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
 		fmt.Printf("Sent %d bytes\n", count)
 	}
-
 }
